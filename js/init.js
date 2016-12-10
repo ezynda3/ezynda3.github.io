491f9ef6eabf62eb56ca7318ea943d28cf67a68e
Ed Zynda <ezynda3@gmail.com>
2016-12-10 10:00:06 -0800

    Use Hugo for site gen now

---
+++ js/init.js
@@ -0,0 +1,10 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+
+    $('.parallax').parallax();
+
+    hljs.initHighlightingOnLoad();
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
